de5af52d50adc41b06da94c0b37708f6aa5a6d17
tedington <tdansa64@gmail.com>
2022-10-17 22:01:52 +0100

    minor changes, updating hero section

--- portfolio_teda/components/Hero.tsx
+++ portfolio_teda/components/Hero.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { Z_FULL_FLUSH } from "zlib";
 import BackgroundCircles from "./BackgroundCircles";
 type Props = {};
 
@@ -7,8 +9,8 @@ export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
       "Fetching metadata of the admin . . .",
-      "Hi, The name's Teda",
-      "Guy_Who_Loves_Computers :)",
+      "Hi, The name's Teda!",
+      "Guy_Who_Loves_Computers&owls  :)",
       "<butLovesToCodeMore/>",
     ],
     loop: true,
@@ -17,10 +19,36 @@ export default function Hero({}: Props) {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="aqua" />
-      </h1>
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://i.pinimg.com/originals/0a/22/42/0a224216772d624d1c2b97d2b2cd31ac.jpg"
+        alt="howl"
+      />
+
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          {" "}
+          Computer Scientist
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="aqua" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
