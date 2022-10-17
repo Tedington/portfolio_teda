9e6608f8c83c7a8f6aa04e3c3b3ef02e95ed3667
tedington <tdansa64@gmail.com>
2022-10-17 11:44:34 +0100

    minor changes

--- portfolio_teda/components/Header.tsx
+++ portfolio_teda/components/Header.tsx
@@ -1,39 +1,44 @@
-import React from 'react'
-import { SocialIcon } from 'react-social-icons'
-
-type Props = {}
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+type Props = {};
 
 function Header({}: Props) {
   return (
-    <header>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center ">
       <div>
         <SocialIcon
-        url="https://twitter.com/Temitope_Dansa"
-        fgColor="grey"
-        bgColor="transparent"
+          url="https://twitter.com/Temitope_Dansa"
+          fgColor="grey"
+          bgColor="transparent"
         />
 
-        <SocialIcon url="https://twitter.com/Temitope_Dansa"
-        fgColor="grey"
-        bgColor="transparent"/>
+        <SocialIcon
+          url="https://twitter.com/Temitope_Dansa"
+          fgColor="grey"
+          bgColor="transparent"
+        />
 
-        <SocialIcon url="https://twitter.com/Temitope_Dansa"
-        fgColor="grey"
-        bgColor="transparent"/>
-
+        <SocialIcon
+          url="https://twitter.com/Temitope_Dansa"
+          fgColor="grey"
+          bgColor="transparent"
+        />
       </div>
 
-      <div>
-      <SocialIcon
-      className="cursor-pointer"
-      network="email"
-      fgColor="grey"
-      bgColor="transparent"
-      />
-
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in touch
+        </p>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
